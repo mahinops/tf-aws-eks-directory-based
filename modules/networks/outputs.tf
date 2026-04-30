3d1aea6e61c4ff07ac7dd63ec486cd30f46fea0f
Mokhlesur Mahin <mokhlesur.mahin@gmail.com>
2026-05-01 04:12:10 +0600

    chore - added outputs for networks

--- modules/networks/outputs.tf
+++ modules/networks/outputs.tf
@@ -14,3 +14,18 @@ output "private_subnet_ids" {
   value = [aws_subnet.subnet-private-1.id, aws_subnet.subnet-private-2.id]
 }
 
+output "internet_gateway_id" {
+  value = aws_internet_gateway.main.id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.main.id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private.id
+}
