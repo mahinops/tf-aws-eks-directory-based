2702d09b8ffaa8f626ec3d207c07d5f660bb2928
Mokhlesur Mahin <mokhlesur.mahin@gmail.com>
2026-05-01 04:09:20 +0600

    feat - rt subnet association

--- modules/networks/main.tf
+++ modules/networks/main.tf
@@ -103,3 +103,24 @@ resource "aws_route_table" "private" {
     Name = "eks-private-rt-${var.environment}"
   }
 }
+
+
+resource "aws_route_table_association" "public-1" {
+  subnet_id      = aws_subnet.subnet-public-1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-2" {
+  subnet_id      = aws_subnet.subnet-public-2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private-1" {
+  subnet_id      = aws_subnet.subnet-private-1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-2" {
+  subnet_id      = aws_subnet.subnet-private-2.id
+  route_table_id = aws_route_table.private.id
+}
