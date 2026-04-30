c3025c1773a3cf08c8f93e5e1bf974a05e2e04f4
Mokhlesur Mahin <mokhlesur.mahin@gmail.com>
2026-05-01 04:02:46 +0600

    fix - public subnet auto assign public ip enabled

--- modules/networks/main.tf
+++ modules/networks/main.tf
@@ -7,9 +7,10 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "subnet-public-1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidr_block_1
-  availability_zone = var.availability_zones[0]
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr_block_1
+  availability_zone       = var.availability_zones[0]
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "eks-subnet-public-1-${var.environment}"
@@ -17,9 +18,10 @@ resource "aws_subnet" "subnet-public-1" {
 }
 
 resource "aws_subnet" "subnet-public-2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidr_block_2
-  availability_zone = var.availability_zones[1]
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr_block_2
+  availability_zone       = var.availability_zones[1]
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "eks-subnet-public-2-${var.environment}"
